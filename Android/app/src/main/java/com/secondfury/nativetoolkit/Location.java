0d7027e5d866d0ea9a6a5c74d7153ada62794201
recih <fengbocih+github@gmail.com>
2019-11-25 00:52:29 +0800

    add checkSelfPermission to Location class

--- Android/app/src/main/java/com/secondfury/nativetoolkit/Location.java
+++ Android/app/src/main/java/com/secondfury/nativetoolkit/Location.java
@@ -1,19 +1,25 @@
 package com.secondfury.nativetoolkit;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.location.LocationManager;
 import android.location.LocationListener;
 
-public class Location implements LocationListener
-{
+import static androidx.core.content.ContextCompat.checkSelfPermission;
+
+public class Location implements LocationListener {
     public static android.location.Location LastLocation;
 
     private LocationManager locationManager;
 
-    public void init(Context context)
-    {
+    public void init(Context context) {
         locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        if (checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this);
         locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 1000, 0, this);
